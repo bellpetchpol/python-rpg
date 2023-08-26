ca7e313d34d7ca114eca2e4cff12c0e56fcec8f7
Tochsaporn Petchpol <tochsaporn@thelivingos.com>
2023-08-26 20:49:54 +0700

    update

--- app/main.py
+++ app/main.py
@@ -1,11 +1,29 @@
 from typing import Annotated
 
-from fastapi import FastAPI, Query
+from fastapi import Body, FastAPI, Path
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 
-@app.get("/items/")
-async def read_items(item_name: Annotated[list[str], Query(include_in_schema=True)] = []):
-    query_items = {"item_name": item_name}
-    return query_items 
+class Item(BaseModel):
+    name: str
+    description: str | None = Field(
+        default=None, max_length=300
+    )
+    price: float = Field(gt=0)
+    tax: float | None = None
+    
+class User(BaseModel):
+    username: str
+    full_name: str | None = None
+
+
+@app.put("/items/{item_id}")
+async def update_item(
+    item_id: Annotated[int, Path(ge=0, le=1000)],
+    item: Annotated[Item, Body(embed=True)]
+    
+):
+    results = {"item_id": item_id, "item": item}
+    return results
